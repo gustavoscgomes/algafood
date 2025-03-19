cfda761244729931ee4d5c5a9fc9b173b6e5eda5
Gustavo Gomes <gugaasousa@gmail.com>
2025-03-19 12:24:29 -0300

    Cria migrations com Flyway

--- src/main/java/br/example/iofood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/example/iofood/domain/repository/RestauranteRepository.java
@@ -17,7 +17,7 @@ public interface RestauranteRepository extends CustomJpaRepository<Restaurante, 
     // esse restaurante não será retornado usando JOIN FETCH r.formasPagamento.
     // Para resolver isso, temos que usar LEFT JOIN FETCH r.formasPagamento
 //	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
-    @Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    @Query("from Restaurante r join fetch r.cozinha")
     List<Restaurante> findAll();
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
